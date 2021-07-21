60d98a7b18f3ffd53ddad0dd58aaff3b967f97d8
chiru <getvenkathere@gmail.com>
2021-07-21 23:32:24 +0530

    Changes made on 07 21 for this controller file

--- src/main/java/com/spring/example/myfirstdemo/controller/MyuserController.java
+++ src/main/java/com/spring/example/myfirstdemo/controller/MyuserController.java
@@ -25,7 +25,8 @@ public class MyuserController {
          details d1 = new details( "Venkata" , "Devops");
          details d2 = new details( "Vignesh" , "Devops");
          details d3 = new details( "Abhijith" , "Devops");
-        details d4 = new details( "Vivek" , "Devops");
+         details d4 = new details( "Vivek" , "Devops");
+        details d5 = new details( "Radha" , "Devops");
          kanhadetails.add(d1);
          kanhadetails.add(d2);
          return kanhadetails;
